709a1d4857f9de6bd9dc357dbd4a71458d266876
andrewCoder2024 <67741322+andrewCoder2024@users.noreply.github.com>
2023-01-23 17:33:39 -0500

    First commit

--- mlapi.py
+++ mlapi.py
@@ -1,19 +1,40 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import pickle
+import json
+import openai
 import pandas as pd
 app = FastAPI()
-class ScoringItem(BaseModel):
-    YearsAtCompany: float
-    EmployeeSatisfaction: float
-    Position: str
-    Salary: int
+import os
+#with open('openaiapikey.txt', 'r') as infile:
+#       openai.api_key = infile.read()
+openai.api_key = os.environ['openai_key']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-with open('rfmodel.pkl', 'rb'):
-    model = pickle.load(f)
+@app.post("")
+async def generate_text(prompt: str):
+    with open('prompt_script.txt', 'r') as infile:
+        prompt = infile.read().replace('<<ED>>', prompt)
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt
+    )
+    completion = response["choices"][0]["text"]
+    lines = completion.split("\n")
+    lines = [line for line in lines if line]
+    data = {}
+    for line in lines:
+        parts = line.split(":")
 
-@app.post('/')
-async def scoring_endpoint(item:ScoringItem):
-    df = pd.DataFrame([item.dict().values()], columns=item.dict().keys())
-    yhat = model.predict(df)
-    return item
+        key = parts[0].strip()
+        value = parts[1].strip()
+
+        data[key] = value
+    json_data = json.dumps(data)
+    return json_data
